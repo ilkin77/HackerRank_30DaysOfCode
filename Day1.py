b08ffe87b49f0f2dc3c8e33dc28513ae0823e4fd
ilkin77 <ilkin78952@gmail.com>
2020-08-28 11:52:30 +0400

    Initial commit

--- Day1.py
+++ Day1.py
@@ -1,6 +1,14 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-input_string = input()
+i = 4
+d = 4.0
+s = 'HackerRank '
 
-# Print a string literal saying "Hello, World." to stdout.
-print('Hello, World.')
-print(input_string)
+a = 0
+b = 0
+
+a = int(input())
+b = float(input())
+c = input()
+
+print(a + i)
+print(b + d)
+print(s + c)
