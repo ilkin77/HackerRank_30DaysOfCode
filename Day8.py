4fb6380a6acbf33ae413d782286a154a406149a1
ilkin77 <ilkin78952@gmail.com>
2020-09-05 01:25:34 +0400

    Initial commit

---
+++ Day8.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+d = dict()
+
+for i in range(0, n):
+    name, number = input().split()
+    d[name] = number
+# print(d)
+
+for i in range(0, n):
+    try:
+        name = input()
+        if name in d:
+            print(f"{name}={d[name]}")
+        else:
+            print("Not found")
+    except:
+        break
